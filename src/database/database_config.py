315e7f0d63a76f2e7ce9021ae46b484b907e196c
Vinícius Amorim <vamorim.dev@gmail.com>
2023-07-04 21:38:27 -0300

    fix: fix database creation
    Co-authored-by: Vinícius Amorim <vamorim.dev@gmail.com>

--- src/database/database_config.py
+++ src/database/database_config.py
@@ -1,16 +1,21 @@
+from __future__ import annotations
+
+import pathlib
 from os import getenv
-from dotenv import load_dotenv
+
 import psycopg2 as pg
+from dotenv import load_dotenv
 from psycopg2 import Error as err
 
 load_dotenv()
 
 # environment variables
-db_host = getenv("DB_HOST")
-db_port = getenv("DB_PORT")
-db_name = getenv("DB_NAME")
-db_user = getenv("DB_USER")
-db_password = getenv("DB_PASSWORD")
+db_host = getenv('DB_HOST')
+db_port = getenv('DB_PORT')
+db_name = getenv('DB_NAME')
+db_user = getenv('DB_USER')
+db_password = getenv('DB_PASSWORD')
+
 
 def create_connection():
     try:
@@ -21,39 +26,35 @@ def create_connection():
             port=db_port,
             dbname=db_name
         )
-        print("Database connected successfully")
+        print('Database connected successfully')
 
     except err:
-        print(f"Database not connected successfully.\n    Error message: {err}")
+        print(f'Database not connected successfully.\n    Error message: {err}')
 
     return conn
 
-def create_tables():
-    conn = create_connection()
-    cur = conn.cursor()
 
+def execute_schema_queries():
     try:
-        # Lendo o arquivo .sql com as queries de criação de tabelas
-        with open('schema.sql', 'r', encoding='utf-8') as sql_file:
-            # Lendo o conteúdo do arquivo
-            queries = sql_file.read()
+        conn = create_connection()
+        cursor = conn.cursor()
 
-            # Executando as queries usando o cursor
-            cur.execute(queries)
-
-        # Commit das alterações
+        # Ler o arquivo schema.sql
+        queries = pathlib.Path('src/database/schema.sql').read_text()
+        # Executar as queries
+        cursor.execute(queries)
         conn.commit()
+        print('Queries executadas com sucesso.')
+        return True
 
-        print("Tabelas criadas com sucesso")
+    except (Exception, err) as error:
+        conn.rollback()
+        print('Erro na execução das queries:', error)
 
-        # Exemplo: Realizando uma consulta na tabela recém-criada
-        cur.execute("SELECT * FROM Assunto")
-        rows = cur.fetchall()
-        for row in rows:
-            print(row)
-
-    except err:
-        print(f"Erro ao criar tabelas.\n    Mensagem de erro: {err}")
-
-    cur.close()
-    conn.close()
+    finally:
+        if conn:
+            # Restaurar o autocommit para True
+            conn.autocommit = True
+            cursor.close()
+            conn.close()
+            print('Conexão fechada.')
