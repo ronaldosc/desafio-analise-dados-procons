001c03b4d526df5579632d5d88cafae8add3b5ca
Vinícius Amorim <vamorim.dev@gmail.com>
2023-07-06 13:51:17 -0300

    feat: add temporal_tendencias.py

--- src/analytics.py
+++ src/analytics.py
@@ -7,6 +7,7 @@ from database.database_config import create_connection
 from flask import Flask
 from flask import render_template
 from flask import request
+from graphs.temporal_tendencias import obter_dados_tendencias
 
 # Conectar ao banco de dados PostgreSQL
 conn = create_connection()
@@ -21,39 +22,8 @@ def index():
 
 @app.route('/tendencias')
 def tendencias():
-    # Consulta ao banco de dados para obter os dados de tendências
-    cur = conn.cursor()
-    cur.execute('SELECT AnoAtendimento, TrimestreAtendimento, MesAtendimento, COUNT(*) AS CountAtendimentos FROM Atendimento GROUP BY AnoAtendimento, TrimestreAtendimento, MesAtendimento ORDER BY AnoAtendimento, TrimestreAtendimento, MesAtendimento')
-    data = cur.fetchall()
-    cur.close()
-
-    # Processar os dados
-    anos = []
-    trimestres = []
-    meses = []
-    contagem = []
-    for row in data:
-        anos.append(row[0])
-        trimestres.append(row[1])
-        meses.append(row[2])
-        contagem.append(row[3])
-
-    # Criar o gráfico de barras verticais com cores diferentes para cada ano
-    fig = go.Figure()
-    for ano in set(anos):
-        indices_ano = [i for i, x in enumerate(anos) if x == ano]
-        cor = f'rgb({random.randint(0, 255)},{random.randint(0, 255)},{random.randint(0, 255)})'
-        fig.add_trace(go.Bar(x=[meses[i] for i in indices_ano], y=[contagem[i]
-                      for i in indices_ano], name=str(ano), marker=dict(color=cor)))
-
-    fig.update_layout(
-        title='Tendências de Atendimentos',
-        xaxis=dict(title='Mês'),
-        yaxis=dict(title='Contagem'),
-        barmode='group'
-    )
-
-    return render_template('tendencias.html', plot=fig.to_html())
+    plot = obter_dados_tendencias()
+    return render_template('tendencias.html', plot=plot)
 
 
 @app.route('/sazonalidade')
