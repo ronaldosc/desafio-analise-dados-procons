619ebbb141f7a784f8bb32c957605285061728f1
Vinícius Amorim <vamorim.dev@gmail.com>
2023-07-06 14:03:34 -0300

    feat: add geografica_regiao_variacao.py

--- src/analytics.py
+++ src/analytics.py
@@ -8,6 +8,7 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from graphs.geografica_regiao_distribuicao import obter_dados_distribuicao_regiao
+from graphs.geografica_regiao_variacao import obter_dados_variacao_tempo
 from graphs.temporal_sazonalidade import obter_dados_sazonalidade
 from graphs.temporal_tendencias import obter_dados_tendencias
 
@@ -42,35 +43,8 @@ def mapa():
 
 @app.route('/variacao-tempo')
 def variacao_tempo():
-    # Consulta ao banco de dados para obter a variação dos atendimentos por região ou UF ao longo do tempo
-    cur = conn.cursor()
-    cur.execute('SELECT DataAtendimento, Regiao, COUNT(*) AS CountAtendimentos FROM Atendimento INNER JOIN Regiao ON Atendimento.CodigoRegiao = Regiao.CodigoRegiao GROUP BY DataAtendimento, Regiao ORDER BY DataAtendimento, Regiao')
-    data = cur.fetchall()
-    cur.close()
-
-    # Processar os dados
-    datas = []
-    regioes = []
-    contagem = []
-    for row in data:
-        datas.append(row[0])
-        regioes.append(row[1])
-        contagem.append(row[2])
-
-    # Criar o gráfico de linhas
-    fig = go.Figure()
-    for regiao in set(regioes):
-        indices_regiao = [i for i, x in enumerate(regioes) if x == regiao]
-        fig.add_trace(go.Scatter(x=[datas[i] for i in indices_regiao], y=[contagem[i]
-                      for i in indices_regiao], mode='lines', name=str(regiao)))
-
-    fig.update_layout(
-        title='Variação dos Atendimentos por Região ou UF',
-        xaxis=dict(title='Data de Atendimento'),
-        yaxis=dict(title='Contagem')
-    )
-
-    return render_template('variacao_tempo.html', plot=fig.to_html())
+    plot = obter_dados_variacao_tempo()
+    return render_template('variacao_tempo.html', plot=plot)
 
 
 @app.route('/assuntos-recorrentes')
